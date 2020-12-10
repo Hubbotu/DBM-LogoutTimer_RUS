48173f3687768050431f7838c9370f43fa7d7efa
Hubbotu <34053496+Hubbotu@users.noreply.github.com>
2020-12-10 08:42:08 +0300

    Add files via upload

---
+++ localization.ru.lua
@@ -0,0 +1,14 @@
+﻿local L = DBM:GetModLocalization("DBM-LogoutTimer")
+
+L:SetGeneralLocalization({
+	name	= "Logout Timer"
+})
+
+L:SetTimerLocalization({
+	TimerLogout	= "Выход из системы: "
+})
+
+L:SetMiscLocalization({
+	IdleMessage		= "Вы сейчас отошли: AFK",
+	UnidleMessage	= "Вы снова за ПК."
+})
